85d6a15276b3bf11df3cd8ec84139cb4a8c0b0cd
Trevor <revolution.x@gmail.com>
2023-09-18 13:44:33 +0900

    Feature/expenses routes (#16)
    * Add proper expenses routes
    
    * Fix some bugs

--- src/routes/expenses.py
+++ src/routes/expenses.py
@@ -5,28 +5,37 @@ from ..models.models import Expense
 
 # Expense
 
+@app.route("/expense", methods=["POST"])
+def add_expense_api():
+    body = request.get_json()
+    new_expense = Expense(**body)
+    return add_object_to_database(new_expense)
+
 
 @app.route("/expenses", methods=["GET"])
 def get_expense_all_api():
     return get_json_array(get_db_entries(Expense))
 
 
-@app.route("/expenses/<int:expenseId>", methods=["GET"])
-def get_expense(expenseId):
-    return "get expenses with ID %s" % expenseId
+@app.route("/expense/<int:expenseId>", methods=["GET"])
+def get_expense_api(expenseId):
+    return get_db_entry_by_id(Expense, expenseId).get_dict()
 
 
 @app.route("/expense/<int:expenseId>", methods=["PATCH"])
-def update_expense(expenseId):
-    return "update expense with ID %s" % expenseId
+def update_expense_api(expenseId):
+    req = request.get_json()
+    s = get_session()
+    s.begin()
+    expense = s.scalars(select(Expense).where(Expense.id == expenseId)).one()
+    update_object_properties(expense, req)
+    s.commit()
+    output = expense.get_dict()
+    s.close()
+    return output
 
 
 @app.route("/expense/<int:expenseId>", methods=["DELETE"])
-def remove_expense(expenseId):
-    return "remove expense with ID %s" % expenseId
-
-@app.route("/expense", methods=["POST"])
-def add_expense_api_test():
-    body = request.get_json()
-    new_expense = Expense(**body)
-    return add_object_to_database(new_expense)
+def remove_expense_api(expenseId):
+    expense =  get_db_entry_by_id(Expense ,expenseId)
+    return delete_object_from_database(expense)
