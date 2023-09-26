7533da9f737520a552ad828c19349465c40119c7
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-09-26 18:08:37 +0800

    added empty-string validation

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from sqlalchemy import (Boolean, Column, DateTime, Integer, String, ForeignKey, func)
-from sqlalchemy.orm import relationship, mapped_column
+from sqlalchemy.orm import relationship, mapped_column, validates
 
 from .base import Base
 
@@ -9,6 +9,10 @@ class Category(Base):
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = mapped_column(String, unique=True, nullable=False)
+
+    @validates('id', 'name')
+    def empty_string_to_null(self, key, value):
+        return None if value == '' else value
 
 class Expense(Base):
     __tablename__ = 'expenses'
@@ -24,6 +28,10 @@ class Expense(Base):
     description = mapped_column(String)
     paid_back = mapped_column(Boolean, default=False)
 
+    @validates('id', 'registered_by_user', 'created_at', 'category', 'expense_date', 'cost', 'paid_back')
+    def empty_string_to_null(self, key, value):
+        return None if value == '' else value
+
     user = relationship(
         'User', primaryjoin='Expense.registered_by_user == User.id', backref='user_expenses')
     category_ref = relationship(
@@ -38,6 +46,10 @@ class User(Base):
     email = mapped_column(String, unique=True)
     created_at = mapped_column(DateTime, server_default=func.now())
 
+    @validates('id', 'auth_provider_id', 'username')
+    def empty_string_to_null(self, key, value):
+        return None if value == '' else value
+
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r}, email={self.email!r}, created_at={self.created_at.strftime('%Y-%m-%d %H:%M:%S')})"
 
@@ -49,3 +61,7 @@ class AccessToken(Base):
     created_at = mapped_column(DateTime, server_default=func.now(), nullable=False)
     expires = mapped_column(DateTime, nullable=False)
     token_value = mapped_column(String, nullable=False)
+
+    @validates('id', 'registered_to_user ', 'created_at', 'expires', 'token_value')
+    def empty_string_to_null(self, key, value):
+        return None if value == '' else value
