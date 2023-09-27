21c10b03fdb8dbd78a2fafd9f46d97849dc3a003
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-09-27 15:41:44 +0800

    moved the validation to a mixin class

--- src/models/models.py
+++ src/models/models.py
@@ -4,17 +4,23 @@ from sqlalchemy.orm import relationship, mapped_column, validates
 
 from .base import Base
 
-class Category(Base):
+class EmptyStringToNullMixin:
+    category_columns = ('id', 'name')
+    expense_columns =  ('id', 'registered_by_user', 'created_at', 'category', 'expense_date', 'cost', 'paid_back')
+    user_columns =     ('id', 'auth_provider_id', 'username')
+    token_columns =    ('id', 'registered_to_user ', 'created_at', 'expires', 'token_value')
+    all_columns = set((*category_columns, *expense_columns, *user_columns, *token_columns))
+    @validates(*all_columns)
+    def empty_string_to_null(self, key, value):
+        return None if value == '' else value
+
+class Category(Base, EmptyStringToNullMixin):
     __tablename__ = 'categories'
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = mapped_column(String, unique=True, nullable=False)
 
-    @validates('id', 'name')
-    def empty_string_to_null(self, key, value):
-        return None if value == '' else value
-
-class Expense(Base):
+class Expense(Base, EmptyStringToNullMixin):
     __tablename__ = 'expenses'
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -28,16 +34,12 @@ class Expense(Base):
     description = mapped_column(String)
     paid_back = mapped_column(Boolean, default=False)
 
-    @validates('id', 'registered_by_user', 'created_at', 'category', 'expense_date', 'cost', 'paid_back')
-    def empty_string_to_null(self, key, value):
-        return None if value == '' else value
-
     user = relationship(
         'User', primaryjoin='Expense.registered_by_user == User.id', backref='user_expenses')
     category_ref = relationship(
         'Category', primaryjoin='Expense.category == Category.id')
 
-class User(Base):
+class User(Base, EmptyStringToNullMixin):
     __tablename__ = 'users'
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -46,14 +48,10 @@ class User(Base):
     email = mapped_column(String, unique=True)
     created_at = mapped_column(DateTime, server_default=func.now())
 
-    @validates('id', 'auth_provider_id', 'username')
-    def empty_string_to_null(self, key, value):
-        return None if value == '' else value
-
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r}, email={self.email!r}, created_at={self.created_at.strftime('%Y-%m-%d %H:%M:%S')})"
 
-class AccessToken(Base):
+class AccessToken(Base, EmptyStringToNullMixin):
     __tablename__ = 'accesstokens'
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -61,7 +59,3 @@ class AccessToken(Base):
     created_at = mapped_column(DateTime, server_default=func.now(), nullable=False)
     expires = mapped_column(DateTime, nullable=False)
     token_value = mapped_column(String, nullable=False)
-
-    @validates('id', 'registered_to_user ', 'created_at', 'expires', 'token_value')
-    def empty_string_to_null(self, key, value):
-        return None if value == '' else value
